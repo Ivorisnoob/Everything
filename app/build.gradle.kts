f04989b3b1bb8df142ca51c70cc836794bfdcf33
Ivorisnoob <harshgamer6351@gmail.com>
2025-12-21 22:02:41 +0530

    version bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.ivors.everything"
         minSdk = 33
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
